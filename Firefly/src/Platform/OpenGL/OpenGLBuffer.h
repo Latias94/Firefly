150d651e4080fbba5ac40bc8703f17c2e3a1e8a1
Latias94 <superfrankie621@gmail.com>
2021-07-11 22:53:02 +0800

    Created Vertex Buffer Layouts.

--- Firefly/src/Platform/OpenGL/OpenGLBuffer.h
+++ Firefly/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,11 +10,16 @@ namespace Firefly
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
+
+        void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
+        const BufferLayout& GetLayout() const override { return m_Layout; }
 
     private:
-        uint32_t m_RendererID;
+        uint32_t     m_RendererID;
+        BufferLayout m_Layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer
@@ -23,9 +28,10 @@ namespace Firefly
         OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
-        virtual uint32_t GetCount() const {return m_Count;}
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
+
+        virtual uint32_t GetCount() const { return m_Count; }
 
     private:
         uint32_t m_RendererID;
